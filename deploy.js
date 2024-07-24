7f1411a3464c1b4ec352c65a51474ff3f09834e2
Tom Wilson <tom@hyper.io>
2024-07-24 00:08:30 +0000

    bump w64 module to latest ao

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
-// import Irys as normal
-import Irys from "@irys/sdk";
+import { TurboFactory, ArweaveSigner } from '@ardrive/turbo-sdk';
 import fs from "fs";
+import path from 'path';
 
 // load Arweave wallet
 const wallet = JSON.parse(
@@ -8,25 +8,25 @@ const wallet = JSON.parse(
 );
 
 const main = async () => {
-  const token = "arweave";
-  const irys = new Irys({
-    url: "https://turbo.ardrive.io", // URL of the node you want to connect to, https://turbo.ardrive.io will facilitate upload using ArDrive Turbo.
-    token, // Token used for payment and signing
-    key: wallet, // Arweave wallet
-  });
-
-  const receipt = await irys.uploadFile("./aos/process/process.wasm", {
-    tags: [
-      { name: 'Content-Type', value: 'application/wasm' },
-      { name: 'Data-Protocol', value: 'ao' },
-      { name: 'Type', value: 'Module' },
-      { name: 'Variant', value: 'ao.TN.1' },
-      { name: 'Module-Format', value: 'wasm64-unknown-emscripten-draft_2024_02_15' },
-      { name: 'Input-Encoding', value: 'JSON-1' },
-      { name: 'Output-Encoding', value: 'JSON-1' },
-      { name: 'Memory-Limit', value: '1-gb' },
-      { name: 'Compute-Limit', value: '9000000000000' }
-    ]
+  const signer = new ArweaveSigner(wallet);
+  const turbo = TurboFactory.authenticated({ signer });
+  const _file = path.resolve('./aos/process/process.wasm')
+  const receipt = await turbo.uploadFile({
+    fileSizeFactory: () => fs.statSync(_file).size,
+    fileStreamFactory: () => fs.createReadStream(_file),
+    dataItemOpts: {
+      tags: [
+        { name: 'Content-Type', value: 'application/wasm' },
+        { name: 'Data-Protocol', value: 'ao' },
+        { name: 'Type', value: 'Module' },
+        { name: 'Variant', value: 'ao.TN.1' },
+        { name: 'Module-Format', value: 'wasm64-unknown-emscripten-draft_2024_02_15' },
+        { name: 'Input-Encoding', value: 'JSON-1' },
+        { name: 'Output-Encoding', value: 'JSON-1' },
+        { name: 'Memory-Limit', value: '1-gb' },
+        { name: 'Compute-Limit', value: '9000000000000' }
+      ]
+    }
   });
   console.log(receipt);
   console.log('ModuleID: ', receipt.id)
